7ebc00fd566fa0cbe2307ab3d1340f74d4e85028
Raphael <raphaelheineck@gmail.com>
2023-05-11 14:51:12 -0300

    final commit

---
+++ scripts/messages.js
@@ -0,0 +1,22 @@
+export const Messages = {
+  title: document.querySelector('.card h1').textContent,
+  openCookieTitle: 'Aqui está sua sorte de hoje:',
+  subtitle: document.querySelector('.card p').textContent,
+  luckyMessage: [
+    "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+    "A vida trará coisas boas se tiver paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+    "Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",
+    "Não importa o tamanho da montanha, ela não pode tapar o sol.",
+    "Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.",
+    "Faça pequenas coisas hoje e coisas maiores lhe serão confiadas amanhã.",
+    "Todas as coisas são difíceis antes de se tornarem fáceis.",
+    "Proibido estacionar nas quartas e sextas",
+    "Na dúvida, não ultrapasse",
+    "Não esqueça de dar um bom sinal nas horas de fome.",
+    "Um cabelo na cabeça é pouco, mas na sopa é muito",
+    "Mais um pássaro voando do que dois na mão"
+  ]
+}
